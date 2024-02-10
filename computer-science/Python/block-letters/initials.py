5c221716635177542557548a0352cbca9a7634b8
Missy <mdowner@southwestrecoveryalliance.onmicrosoft.com>
2024-02-10 08:19:04 +0000

    upload block letters project

---
+++ computer-science/Python/block-letters/initials.py
@@ -0,0 +1,14 @@
+# I'm Missy Downer.
+# There is nothing fun about me.
+
+name = """
+M   M  DDDD
+MM MM  D   D
+MM MM  D   D
+M M M  D   D
+M   M  D   D
+M   M  D   D
+M   M  DDDD
+"""
+
+print(name)
