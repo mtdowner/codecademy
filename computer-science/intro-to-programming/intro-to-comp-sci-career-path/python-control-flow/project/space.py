c6f54756d703adc55aab34196e51c8d75d48fffc
Missy <mdowner@southwestrecoveryalliance.onmicrosoft.com>
2024-03-31 06:58:36 +0000

    update directories

--- computer-science/intro-to-programming/intro-to-comp-sci-career-path/python-control-flow/project/space.py
+++ computer-science/intro-to-programming/intro-to-comp-sci-career-path/python-control-flow/project/space.py
@@ -3,8 +3,8 @@ print("I have information for the following planets:\n")
 print("   1. Venus   2. Mars    3. Jupiter")
 print("   4. Saturn  5. Uranus  6. Neptune\n")
  
-weight = 185
-planet = 3
+weight = 125
+planet = 6
 
 # Write an if statement below:
 
@@ -23,4 +23,4 @@ if planet == 6:
 
 print("Drumroll please!")
 print("...")
-print("Your new weight is: ", weight)
+print("Your new weight is: ", weight, "lbs")
